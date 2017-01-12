edf17f8fa9a0ae7a3d7f2acb8d98da835877761b
Andy Edwards <andyhedwards@gmail.com>
2017-01-12 22:40:16 +1100

    Initialise repo with ember-data, sass, bourbon, breakpoint and autoprefixer

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,15 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: [
+        'bower_components/breakpoint-sass/stylesheets'
+      ]
+    },
+    autoprefixer: {
+      browsers: ['last 2 version'],
+      cascade: false
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
